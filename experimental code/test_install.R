e3472fb86d0670dee3e33f11df99f005fb90c1f6
HarrisQ <harris.quach@gmail.com>
2020-12-17 21:07:05 -0500

    Update test_install.R

--- experimental code/test_install.R
+++ experimental code/test_install.R
@@ -5,4 +5,13 @@ if (!require(devtools)) install.packages("devtools");
 # Installing package "linearsdr" from github;
 devtools::install_github("HarrisQ/linearsdr");
 
-# Generate some data
+# Generate some discrete data
+
+
+# Test OPCG, RADE, MADE with plots and distance
+
+
+# Generate some continuous data
+
+
+# Test OPCG, RADE, MADE with plots and distance
