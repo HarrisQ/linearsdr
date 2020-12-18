6e1fdac92c67a607ee2a45be7878a649309adea1
HarrisQ <harris.quach@gmail.com>
2020-12-17 21:06:57 -0500

    Create test_install.R

---
+++ experimental code/test_install.R
@@ -0,0 +1,8 @@
+
+# Check if devtools is installed
+if (!require(devtools)) install.packages("devtools");
+
+# Installing package "linearsdr" from github;
+devtools::install_github("HarrisQ/linearsdr");
+
+# Generate some data
