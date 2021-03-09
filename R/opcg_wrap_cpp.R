bb4b23cf9e7f000852984ba7cc0e7621187cb40c
HarrisQ <harris.quach@gmail.com>
2021-03-09 17:56:14 -0500

    Update opcg_wrap_cpp.R
    fixed wolfe1 type/error

--- R/opcg_wrap_cpp.R
+++ R/opcg_wrap_cpp.R
@@ -52,7 +52,7 @@ opcg_made <- function(x_matrix, y_matrix, bw, B_mat=NULL, ytype='continuous',
   beta_bt1=if ( "beta_bt1" %in% control_names ) control_args$beta_bt1 else 0.5;
   c_ag1=if ( "c_ag1" %in% control_names ) control_args$c_ag1 else 1e-3; #1e-3 too small?
   c_ag2=if ( "c_ag2" %in% control_names ) control_args$c_ag2 else 0;
-  c_wolfe1=if ( "c_wolfe" %in% control_names ) control_args$c_wolfe1 else 0; 
+  c_wolfe1=if ( "c_wolfe" %in% control_names ) control_args$c_wolfe else 0; 
   max_iter_line1=if ( "max_iter_line1" %in% control_names ) control_args$max_iter_line1 else 100; 
   l2_pen=if ( "l2_pen" %in% control_names ) control_args$l2_pen else 0;  
   
@@ -140,7 +140,7 @@ opcg_made <- function(x_matrix, y_matrix, bw, B_mat=NULL, ytype='continuous',
     }
     
     
-    # j=150; test=T
+    # j=1; test=T
     aD_j = function(j, test=F) {
       
       # centering data at obs j 
