51ef81632818732563b3601522ec6d6155ef2270
HarrisQ <harris.quach@gmail.com>
2021-04-22 12:51:56 -0400

    Update opcg_wrap_cpp.R

--- R/opcg_wrap_cpp.R
+++ R/opcg_wrap_cpp.R
@@ -139,7 +139,7 @@ opcg_made <- function(x_matrix, y_matrix, bw, B_mat=NULL, ytype='continuous',
       # emp_adcat( mv_Y[,980:1000], k_vec, tune=0.05 );
     } else if (ytype %in% c("clogit", "cprobit", "cloglog" ) ) { 
       mv_Y = linearsdr:::mnY_to_mvY( y_matrix, m_classes, ytype="ord-cat");
-      linktype="clogit";
+      linktype=ytype;
       
       k_vec = rep(1, n) #as.vector(y_matrix);
       mv_Y=matrix(mv_Y[2:(m),], m-1, n) # Drop the first row now cause its all 1
