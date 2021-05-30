88fd338cd8766b48a14ec49c48189c24d607eb16
HarrisQ <harris.quach@gmail.com>
2021-05-30 15:27:15 -0400

    Update mn_loss_funs.cpp

--- src/mn_loss_funs.cpp
+++ src/mn_loss_funs.cpp
@@ -270,7 +270,7 @@ arma::mat mn_loss_j(arma::vec c,
   arma::mat slope_mat(p,m); 
   arma::mat slope_mat = reshape(c.rows( m, pm-1 ), p, m);
                          
-  arma::mat pen_term(1,1); pen_term = lambda*trace( slope_mat*slope_mat.t() )
+  arma::mat pen_term(1,1); pen_term = lambda*trace( slope_mat*slope_mat.t() );
   
   
   if (link=="expit"){
