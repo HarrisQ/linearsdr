7d756ecfabfd4db8fc1a120237c9f7df52e3cc99
HarrisQ <harris.quach@gmail.com>
2021-11-11 02:34:19 -0500

    updated namespace
    matpower added

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // center_cpp
 Rcpp::NumericVector center_cpp(Rcpp::NumericVector x, Rcpp::Nullable<Rcpp::NumericVector> center);
 RcppExport SEXP _linearsdr_center_cpp(SEXP xSEXP, SEXP centerSEXP) {
