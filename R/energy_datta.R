8e37e986b46af5abeff5138cb6cbbc5ddc5d91cc
HarrisQ <harris.quach@gmail.com>
2021-11-12 01:43:54 -0500

    added more datasets and vignettes

---
+++ R/energy_datta.R
@@ -0,0 +1,11 @@
+#' @title Scores of Group A and Group B
+#'
+#' @description A data set with the scores of two groups.
+#'
+#' @format A data frame with 60 rows and 2 variables:
+#' \describe{
+#'   \item{group}{Participant's group, A or B.}
+#'   \item{score}{The participant's score in hypothetical task.}
+#' }
+#' @source <https://archive.ics.uci.edu/ml/datasets/Energy+efficiency>
+"energy_datta"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,42 @@
-## code to prepare `airfoil_datta` dataset goes here
+## code to prepare datasets goes here
+set.seed(2021)
 
+#### NASA Airfoil Dataset ----
 airfoil_datta = read.table("data-raw/airfoil_self_noise.dat", header=F, skip=0)
 
-
 usethis::use_data(airfoil_datta, overwrite = TRUE)
+
+#### Energy Efficiency Dataset ----
+# Bivariate Y
+
+energy_datta = readxl::read_xlsx("data-raw/ENB2012_data.xlsx")
+
+usethis::use_data(energy_datta, overwrite = TRUE)
+
+#### Superconductor Dataset ----
+
+supercond_datta_entire = read.csv2("data-raw/superconductor.csv", header = T, sep=",")
+dim(supercond_datta_entire)
+
+
+sample_set = sample(1:dim(supercond_datta_entire)[1], 500)
+supercond_datta = supercond_datta_entire[sample_set,]
+supercond_datta = sapply(1:dim(supercond_datta)[2], 
+                         function(k) as.numeric(supercond_datta[,k]))
+
+usethis::use_data(supercond_datta, overwrite = TRUE)
+
+#### Communities and Crime Dataset ----
+
+crime_datta_entire=as.data.frame(read.csv('data-raw/communities.data', header = F, sep = ','))
+# dim(crime_datta_entire)
+# Drop first 5; drop NA; 102-118, 122-125, 127
+crime_datta = crime_datta_entire[,-c(1:5, 31,102:118, 122:125, 127)]
+
+sample_set = sample(1:dim(crime_datta)[1], 500)
+crime_datta = crime_datta[sample_set,]
+
+crime_datta = sapply(1:dim(crime_datta)[2], 
+                     function(k) as.numeric(crime_datta[,k]))
+
+usethis::use_data(crime_datta, overwrite = TRUE)
