7d029703b3a10a9dd8b60d7a373fbb3066436385
HarrisQ <harris.quach@gmail.com>
2021-11-13 19:19:16 -0500

    Update opcg_wrap.cpp

--- src/opcg_wrap.cpp
+++ src/opcg_wrap.cpp
@@ -428,8 +428,8 @@ arma::vec vecB_cg(arma::vec init,
   arma::vec grad_now;
   
   if (link == "continuous"){
-    nll_now = mgauss_loss_made(c_now,x_datta,y_datta,bw,ahat_list, Dhat_list, r_mat);
-    grad_now = mgauss_score_made(c_now,x_datta,y_datta,bw,ahat_list, Dhat_list, r_mat);
+    // nll_now = mgauss_loss_made(c_now,x_datta,y_datta,bw,ahat_list, Dhat_list, r_mat);
+    // grad_now = mgauss_score_made(c_now,x_datta,y_datta,bw,ahat_list, Dhat_list, r_mat);
   } else {
     nll_now = mn_loss_made(c_now,x_datta,y_datta,bw,ahat_list, Dhat_list,link,k,r_mat);
     grad_now = mn_score_made(c_now,x_datta,y_datta,bw,ahat_list, Dhat_list,link,k,r_mat);
