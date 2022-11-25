e07993d3f3fc2c07a8597f21c56559289fb90b8e
HarrisQ <harris.quach@gmail.com>
2022-11-25 17:15:14 -0500

    Update mn_loss_funs.cpp

--- src/mn_loss_funs.cpp
+++ src/mn_loss_funs.cpp
@@ -444,10 +444,10 @@ arma::mat mn_score_j(arma::vec c,
       
       // dot tau 
       // arma::mat tau_tmp = tau*tau.t(); 
-      arma::mat dot_tau = pinv(dot_eta); //tau_tmp - diagmat(tau); 
+      // arma::mat dot_tau = pinv(dot_eta); //tau_tmp - diagmat(tau); 
       
       // W = dot tau inv(V_tau) dot_tau
-      arma::mat W = (dot_tau.t()*pinv(E_syml - tau*tau.t())*dot_tau);
+      arma::mat W = pinv(dot_eta.t()*(E_syml - tau*tau.t())*dot_eta);
       
       mean_score_j += -wj(i)*tVij_I.t()*(dot_eta)*W*( y_datta.col(i) - tau)/n;  
     }
@@ -633,10 +633,10 @@ arma::mat mn_info_j(arma::vec c,
       
       // dot tau 
       // arma::mat tau_tmp = tau*tau.t(); 
-      arma::mat dot_tau = pinv(dot_eta); //tau_tmp - diagmat(tau);  
+      // arma::mat dot_tau = pinv(dot_eta); //tau_tmp - diagmat(tau);  
       
       // W = inv(dot psi V_tau dot_psi)
-      arma::mat W = (dot_tau.t()*pinv(E_syml - tau*tau.t())*dot_tau);      
+      arma::mat W = pinv(dot_eta.t()*(E_syml - tau*tau.t())*dot_eta);      
       
       mean_info_j += wj(i)*tVij_I.t()*W*tVij_I/n;
       
